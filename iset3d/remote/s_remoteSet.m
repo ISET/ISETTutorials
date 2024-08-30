9e21c721d0d1ed0852cbece9550e9737481de0f7
Brian Wandell <wandell@stanford.edu>
2024-08-30 08:58:03 -0700

    Moving tutorials from iset3d-tiny

---
+++ iset3d/remote/s_remoteSet.m
@@ -0,0 +1,12 @@
+%% Illustrates how to configure Matlab prefs for different remote computers
+%
+% There is now a method in isetdocker method that asks a few questions
+% to implement this process.
+%
+
+doc isetdocker
+
+return;
+
+%% END
+
